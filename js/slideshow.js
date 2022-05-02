a6062e2cf2d3b501dc228570c6a8d733f4bc96d5
kailaho <kaho3670@colorado.edu>
2022-05-02 15:08:14 -0600

    first final push

---
+++ js/slideshow.js
@@ -0,0 +1,16 @@
+var img1src = "./img/fishVans.jpg";
+var img2src= "./img/outkast.jpg";
+var img3src= "./img/flowers.jpg";
+var img1alt = "Koi Fish Vans";
+var img2alt = "OutKast Poster";
+var img3alt = "Poppy Poster";
+
+const blank = document.querySelector('sildeShow');
+const context = blank.getConext('2d');
+
+function nextImage(){
+
+  for(var i = 0; i < 3; i++){
+    
+  }
+}
